da1d6ef2780b3251ef3aad2e33f9e12731e48d74
Zach <zach.hathaway48@gmail.com>
2019-04-27 14:48:05 -0400

    Updated code.

--- fifa.py
+++ fifa.py
@@ -1,19 +1,43 @@
+# Import libraries
 import pandas as pd
 import numpy as np
+import team_stats
 
+
+"""
+Read in FIFA data
+
+Returns:
+    fifa_player_df      FIFA player data (Pandas dataframe)
+    fifa_team_df        FIFA team data (Pandas dataframe)
+"""
 def read_data():
     # Player data
     fifa_player_df = pd.read_csv("fifa_player.csv")
+    fifa_player_df = fifa_player_df.drop(['ID', 'Photo', 'Flag', 'Club Logo', 'Special', 'Real Face', 'Jersey Number', 'Joined', 'Contract Valid Until', 'Release Clause'], axis=1)
 
-    # Team data - Need to drop every other row
+    # Team data
     fifa_team_df = pd.read_csv("fifa_team.csv")
-    fifa_team_df = fifa_team_df.drop("", axis=0)
+
+    return fifa_player_df, fifa_team_df
+
+
+"""
+Join player and team FIFA data based on team names
+
+Arguments:
+    team_data       FIFA team data (Pandas dataframe)
+    player_data     FIFA player data (Pandas dataframe)
+"""
 
 def match_teams(team_data, player_data):
-    pass
+    team_data = team_data.drop_duplicates('Subteam')
+    print(team_data)
+
 
 def main():
-    pass
+    fifa_player_df, fifa_team_df = read_data()
+    match_teams(fifa_team_df, fifa_player_df)
 
 if __name__ == "__main__":
     main()
--- team_stats.py
+++ team_stats.py
@@ -1,8 +1,12 @@
+# Import libraries
 import numpy as np
 
 
 teams = {}
 
+"""
+FIFA Team
+"""
 class Team:
 	class Year:
 		def __init__(self, y):
@@ -16,12 +20,13 @@ class Team:
 		self.name = name
 		self.league = league
 		self.years = {}
+		self.players = {}
 		return
 
 	def win(self, year):
 		self.years[year].wins += 1
 		return
-	
+
 	def loss(self, year):
 		self.years[year].loss += 1
 		return
@@ -29,6 +34,17 @@ class Team:
 	def draw(self, year):
 		self.years[year].draws += 1
 		return
+
+	def add_player(self, player):
+		self.players[player.name] = player
+
+	def aggregate_stats(self):
+		results = {}
+		for stat in Player.stat_names:
+			best_player_name, best_player = max(self.players.items(), key=lambda name, player: player.abilities[stat])
+			team_avg = np.mean(player.abilities[stat] for name, player in self.players.items() if name != best_player_name)
+			result[stat] = np.mean(best_player.abilities[stat], team_avg)
+	    return results
 
 	def w2file(self, wfile):
 		for key, y in self.years.items():
@@ -40,7 +56,34 @@ class Team:
 			twrite += str(y.draws) + "\n"
 			wfile.write(twrite)
 		return
-	
+
+
+"""
+FIFA Player
+"""
+class Player:
+	stat_names = ['Crossing', 'Finishing', 'HeadingAccuracy', 'ShortPassing', 'Volleys', 'Dribbling',
+       'Curve', 'FKAccuracy', 'LongPassing', 'BallControl', 'Acceleration',
+       'SprintSpeed', 'Agility', 'Reactions', 'Balance', 'ShotPower',
+       'Jumping', 'Stamina', 'Strength', 'LongShots', 'Aggression',
+       'Interceptions', 'Positioning', 'Vision', 'Penalties', 'Composure',
+       'Marking', 'StandingTackle', 'SlidingTackle', 'GKDiving', 'GKHandling',
+       'GKKicking', 'GKPositioning', 'GKReflexes']
+
+	def __init__(self, stats):
+		# Pass stats as a dataframe row
+		self.name = stats['Name']
+		self.team = stats['Club']
+
+		self.abilities = {}
+		for stat in stat_names:
+			self.abilities[stat] = stats[stat]
+
+		if stats['Position'] is 'GK':
+			self.goalkeeper = True
+		else:
+			self.goalkeeper = False
+
 
 def add_team(teams, name, league):
 	teams[name] = Team(name, league)
@@ -50,53 +93,55 @@ def add_year(teams, name, year):
 	teams[name].years[year] = Team.Year(year)
 	return
 
-stats = open("spi_matches.csv", "r")
 
+"""
+Create FIFA Team from FiveThirtyEight data
+"""
+def create_team_from_data():
+	stats = open("spi_matches.csv", "r")
 
-for line in stats:
-	cv = line.split(",")
+	for line in stats:
+		cv = line.split(",")
 
-	# 14 and 15 for score
-	score1 = cv[14]
-	score2 = cv[15]
+		# 14 and 15 for score
+		score1 = cv[14]
+		score2 = cv[15]
 
-	if score1 == '' or score2 == '':
-		continue
+		if score1 == '' or score2 == '':
+			continue
 
-	team1 = cv[3]
-	team2 = cv[4]
-	league = cv[2]
-	if team1 not in teams:
-		add_team(teams, team1, league)
+		team1 = cv[3]
+		team2 = cv[4]
+		league = cv[2]
+		if team1 not in teams:
+			add_team(teams, team1, league)
 
-	if team2 not in teams:
-		add_team(teams, team2, league)
+		if team2 not in teams:
+			add_team(teams, team2, league)
 
-	date = cv[0].split("-")
-	year = date[0]
+		date = cv[0].split("-")
+		year = date[0]
 
-	if year not in teams[team1].years:
-		add_year(teams, team1, year)
+		if year not in teams[team1].years:
+			add_year(teams, team1, year)
 
-	if year not in teams[team2].years:
-		add_year(teams, team2, year)
+		if year not in teams[team2].years:
+			add_year(teams, team2, year)
 
-	if score1 > score2:
-		teams[team1].win(year)	#.wins += 1
-		teams[team2].loss(year) #loss += 1
-	elif score1 < score2:
-		teams[team1].loss(year) # += 1
-		teams[team2].win(year) # += 1
-	elif score1 == score2:
-		teams[team1].draw(year) #s += 1
-		teams[team2].draw(year) #s += 1
+		if score1 > score2:
+			teams[team1].win(year)	#.wins += 1
+			teams[team2].loss(year) #loss += 1
+		elif score1 < score2:
+			teams[team1].loss(year) # += 1
+			teams[team2].win(year) # += 1
+		elif score1 == score2:
+			teams[team1].draw(year) #s += 1
+			teams[team2].draw(year) #s += 1
 
-wfile = open("new_win_loss.csv", "w")
+	wfile = open("fifa_team.csv", "w")
 
-for key, team in teams.items():
-	team.w2file(wfile)
+	for key, team in teams.items():
+		team.w2file(wfile)
 
-stats.close()
-wfile.close()
-
-
+	stats.close()
+	wfile.close()
