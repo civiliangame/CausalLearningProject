3c8f31b5fa85866d1712009561e6bd258daac294
Arthur Xin <arthur@Arthurs-MacBook-Air.local>
2019-05-01 16:27:30 -0400

    updated code without interpretation

--- analysis.r
+++ analysis.r
@@ -1,11 +1,14 @@
 library("ipw")
 library("magrittr")
+library("rlist")
 library("tidyverse")
 library("survey")
 library("WaveletComp")
 
 ########################################################################################################################
+####################################################               #####################################################
 #################################################### DATA CLEANING #####################################################
+####################################################               #####################################################
 ########################################################################################################################
 
 stat_names <- c('Crossing', 'Finishing', 'HeadingAccuracy', 'ShortPassing', 'Volleys', 'Dribbling',
@@ -29,18 +32,6 @@ data.work <- data.raw[, !names(data.raw) %in% c("name", "win_rate")]
 #unique_work_rate <- data.raw$work_rate %>% unique()
 unique_leagues <- data.raw$league %>% unique()
 
-#separate tables by work rate of 0 and 1
-#data.work_zero <- data.work[data.work$work_rate == 0,]
-#data.work_one <- data.work[data.work$work_rate == 1,]
-
-#Unique leagues for work rates of 0 and 1
-#unique_league_zero <- data.work_zero$league %>% unique()
-#unique_league_one  <- data.work_one$league %>% unique()
-
-#ipw for work rate of one
-#ipw.one <- ipwpoint(exposure = win_num, family = "binomial", link = "logit", denominator = ~1, data = data.work_one)
-#weights.one <- ipw.one$ipw.weights
-
 #create the league table
 df.league <- data.frame(X = data.raw$X) #adds in the X as placeholder for number of desired columns
 for(i in 1:length(unique_leagues)){
@@ -59,7 +50,7 @@ names(df.league) <- c( "X",
 )
 
 #names(df.league) <- c("X", unique_leagues %>% as.character()) #applies new names
-df.combined <- left_join(data.work, df.league)
+df.combined <- left_join(data.work, df.league, by = "X")
 
 #testing glm
 #print(unique_leagues)
@@ -70,126 +61,114 @@ skillNames <- colnames(df.combined)[2:35]
 #sum up skills and adds the aggregated column
 df.combined %<>% mutate(AggregateSkill = df.combined[,2:35] %>% rowSums())
 
-########################################################################################################################
-################################################# BEGINS IP WEIGHTING ##################################################
-########################################################################################################################
-
-#creates the ip weighted model based on aggregate skill and stratified by leagues
-weightedModel <- ipwpoint(exposure = AggregateSkill,
-    family = "gaussian",
-    numerator = ~1,
-    denominator = ~ 
-      EnglishLeagueOne+
-      MajorLeagueSoccer+
-      UEFAEuropaLeague+
-      SpanishPrimeraDivision+
-      ItalySerieA+
-      DutchEredivisie+
-      TurkishTurkcellSuperLig+
-      ArgentinaPrimeraDivision+
-      FrenchLigue1+
-      SpanishSegundaDivision+
-      GermanBundesliga+
-      PortugueseLiga+
-      SouthAfricanABSAPremierLeague+
-      BrasileiroSerieA+
-      UEFAChampionsLeague,
-    data = df.combined
-)
-
-#summary statistics for our weighted model
-weightedModel.summary <- summary(weightedModel$ipw.weights)
-#weightedModel.summary
-
-#creates a density plot of the ip weights
-weightedModel.plot <- ipwplot(weights = weightedModel$ipw.weights, logscale = FALSE, main = "weights")
-weightedModel.plot
-
 #further reduces the existing dataframe to remove the "league" non-numeric column
-df.reduced <- df.combined[,!names(df.combined) %in% c("league")] #%>% as.matrix()
+df.reduced <- df.combined[,!names(df.combined) %in% c("league")] 
 
-########################################################################################################################
-######################################## CREATES THE MARGINAL STRUCTURAL MODEL #########################################
-########################################################################################################################
+df.gauss <- df.reduced
 
-#msm <- svyglm(WinRate ~ win_num, design = svydesign(id = ~1, weights = weightedModel$ipw.weights, data = df.reduced))
-dsgn <- svydesign(id = ~1, weights = ~weightedModel$ipw.weights, data = df.combined)
-msm <- svyglm(win_num ~ 
-                AggregateSkill,#+
-                # EnglishLeagueOne+
-                # MajorLeagueSoccer+
-                # BarclaysPremierLeague+
-                # UEFAEuropaLeague+
-                # SwissRaiffeisenSuperLeague+
-                # SpanishPrimeraDivision+
-                # ItalySerieA+
-                # DutchEredivisie+
-                # TurkishTurkcellSuperLig+
-                # AustrianTMobileBundesliga+
-                # ArgentinaPrimeraDivision+
-                # FrenchLigue1+
-                # SpanishSegundaDivision+
-                # GermanBundesliga+
-                # AustralianALeague+
-                # EnglishLeagueChampionship+
-                # PortugueseLiga+
-                # SouthAfricanABSAPremierLeague+
-                # BrasileiroSerieA+
-                # UEFAChampionsLeague+
-                # ChineseSuperLeague+
-                # RussianPremierLiga,
-                design = dsgn)
+#cleaning skills 
+for(i in 2:35){
+  column <- df.reduced[,i]
+  change <- df.reduced[,i]
+  avg <- df.reduced[,i] %>% mean()
+  
+  # greater than mean gets 1
+  change[change > avg] <- 101
+  
+  # less mean gets 0
+  change[change <= avg] <- 0
+  
+  change[change == 101] <- 1
+  
+  df.reduced[,i] <- change
+}
 
 
-coef(msm)
-confint(msm)
+
+
+
+
+
+
 
 
 ########################################################################################################################
-####################################################### OLD CODE #######################################################
+#################################################                       ################################################
+################################################# IP WEIGHTING FUNCTION ################################################
+#################################################                       ################################################
 ########################################################################################################################
 
 
-#length(data.raw$win_num[data.raw$win_num == 1])
-#length(data.raw$win_num[data.raw$win_num == 0])
+msm_gen <- function(df, skillCol, modelName = "binomial"){
+  #ipweight
+  df %<>% mutate(skillCol)
+  if(modelName == "binomial"){
+    ipw.model <- ipwpoint(exposure = skillCol, family = "binomial",
+                        link = "logit",
+                        numerator = ~1, 
+                        denominator = ~ EnglishLeagueOne+ MajorLeagueSoccer+ UEFAEuropaLeague+ SpanishPrimeraDivision+
+                        ItalySerieA+ DutchEredivisie+ TurkishTurkcellSuperLig+ ArgentinaPrimeraDivision+
+                        FrenchLigue1+ SpanishSegundaDivision+ GermanBundesliga+ PortugueseLiga+
+                        SouthAfricanABSAPremierLeague+ BrasileiroSerieA+ UEFAChampionsLeague+ work_rate,
+                        data = df
+    )
+  } else if(modelName == "gaussian"){
+    ipw.model <- ipwpoint(exposure = skillCol, family = "gaussian",
+                          numerator = ~1, 
+                          denominator = ~ EnglishLeagueOne+ MajorLeagueSoccer+ UEFAEuropaLeague+ SpanishPrimeraDivision+
+                            ItalySerieA+ DutchEredivisie+ TurkishTurkcellSuperLig+ ArgentinaPrimeraDivision+
+                            FrenchLigue1+ SpanishSegundaDivision+ GermanBundesliga+ PortugueseLiga+
+                            SouthAfricanABSAPremierLeague+ BrasileiroSerieA+ UEFAChampionsLeague+ work_rate,
+                          data = df
+    )
+  }
+  
+  #ipwsummary
+  ipw.summary <- summary(ipw.model$ipw.weights)
+  
+  #dsgn
+  dsgn <- svydesign(id = ~1, weights = ~ipw.model$ipw.weights, data = df)
+  
+  #msm
+  msm <- svyglm(win_num ~ skillCol, design = dsgn)
+  
+  #coefficients
+  coef <- coef(msm)
+  
+  #confidence interval
+  confint <- confint(msm)
+  
+  return (list(ipw.model, msm, coef, confint))
+}
+
+########################################################################################################################
+#####################################                                  #################################################
+#####################################  ANALYSIS OF IP WEIGHTED MODELS  #################################################
+#####################################                                  #################################################
+########################################################################################################################
+
+#for loop over different skills
+models.gauss <- list()
+models.binom <- list()
 
 
+for(i in 2:35){
+  skillCol <- df.reduced[,i]
+  models.gauss %<>%list.append(msm_gen(df.gauss, skillCol = skillCol, "gaussian"))
+  models.binom %<>%list.append(msm_gen(df.reduced, skillCol = skillCol, "binomial"))
+}
 
 
+gauss <- models.gauss[[2]]
+binom <- models.binom[[1]]
 
+g <- gauss[[2]]
 
+v <- predict(g)
+v2 <- as.data.frame(v)
 
-
-
-
-#for(i in unique_league_zero){
-
-#data.work.i <- data.work_zero[data.work_zero$league == unique_league_one[2],]
-#data.work.i <- data.work.i[, !names(data.work.i) %in% c("league", "name", "win_rate")]
-#one <- data.work.i[1,] %>% unlist() %>% unname()
-#two <- data.work.i[2,] %>% unlist() %>% unname()
-#bruh <- glm(one ~ two)
-#anova(bruh)
-
-#summary(bruh)
-
-#ipwpoint(bruh, family = "ordinal")
-
-
-#}
-
-
-
-
-
-
-#for (wr in csv$work_rate)
-##{
-#  wr_data <- csv$work_rate == wr
-#  for (league in csv[wr_data]$league)
-##  {
-#    for (stat in csv[league]$)
-#    linear_model <- glm(win_rate ~ )
-#    logistic_model <- glm(win_rate ~)
-#  }
-#}
+plot(df.reduced$win_num, v2$link)
+ 
+  
+  
+  
