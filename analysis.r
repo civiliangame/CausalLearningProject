fa5facdf1bef38564bf0bff1c9ea0a791efb68ad
Zach <zach.hathaway48@gmail.com>
2019-05-08 14:50:45 -0400

    Added new graphs. Updated code to show top three skills.

--- analysis.r
+++ analysis.r
@@ -245,61 +245,76 @@ bCausSvy <- causality(models.binom)
 gCaus <- causality(rs_gauss)
 bCaus <- causality(rs_binom)
 
-############################################
-################# PLOTTING #################
-############################################
+##################################################
+#################### PLOTTING ####################
+##################################################
 
 # GLM: Gaussian vs Binomial Models
 gData <- data.frame(type=matrix("GGLM", ncol=1, nrow=34), skill=skillNames, xPoint=1:34, yPoint=gCaus[[1]])
 bData <- data.frame(type=matrix("LGLM", ncol=1, nrow=34), skill=skillNames, xPoint=1:34, yPoint=bCaus[[1]])
 gbData <- rbind(gData, bData)
+gbData <- gbData[order(gbData$yPoint),]
 p <- ggplot(data = gbData,
             mapping = aes(x = xPoint, y = yPoint, color=type))
 
-
+glmDataInd <- gbData$type == 'GGLM' 
 glm_mets <- p + geom_point() + 
   labs(x = "x", y = "GLM: Expected Difference", title = "GLM Model") +
-  geom_text_repel(data = subset(gbData, yPoint == max(yPoint) & (type == "LGLM" | type=="GGLM")),
+  geom_text_repel(data = subset(gbData, yPoint %in% gbData$yPoint[glmDataInd][32:34] | yPoint %in% gbData$yPoint[!glmDataInd][32:34]),
                   mapping = aes(label = skill))
+  #geom_text_repel(data = subset(gbData, yPoint == max(gbData$yPoint[glmDataInd]) | yPoint == max(gbData$yPoint[!glmDataInd])),
+                  #mapping = aes(label = skill))
 glm_mets
 
 # Survey: Gaussian vs Bionmial Models
 gSvyData <- data.frame(type=matrix("GSVY", ncol=1, nrow=34), skill= skillNames, xPoint=1:34, yPoint=gCausSvy[[1]])
 bSvyData <- data.frame(type=matrix("LSVY", ncol=1, nrow=34), skill= skillNames, xPoint=1:34, yPoint=bCausSvy[[1]])
 gbSvyData <- rbind(gSvyData, bSvyData)
+gbSvyData <- gbSvyData[order(gbSvyData$yPoint),]
 
+gbSvyInd <- gbSvyData$type == 'GSVY'
 s <- ggplot(data = gbSvyData,
             mapping = aes(x = xPoint, y = yPoint, color=type))
 
 svy_mets <- s + geom_point() + 
   labs(x = "x", y = "Svy: Expected Difference", title = "Survey Model") +
-  geom_text_repel(data = subset(gbSvyData, yPoint == max(yPoint)),
+  geom_text_repel(data = subset(gbSvyData, yPoint %in% gbSvyData$yPoint[gbSvyInd][32:34] | yPoint %in% gbSvyData$yPoint[!gbSvyInd][32:34]),
                   mapping = aes(label = skill))
+  #geom_text_repel(data = subset(gbSvyData, yPoint == max(gbSvyData$yPoint[gbSvyInd]) | yPoint == max(gbSvyData$yPoint[!gbSvyInd])),
+  #                mapping = aes(label = skill))
 svy_mets
 
 # GLM vs Survey for Gaussian Distro
 gsData <- rbind(gData, gSvyData)
-
+gsData <- gsData[order(gsData$yPoint),]
+gsInd <- gsData$type == 'GGLM'
 gs <- ggplot(data = gsData,
              mapping = aes(x = xPoint, y = yPoint, color = type))
 
 gs_graph <- gs + geom_point() + 
   labs(x = "x", y = "Expected Difference", title = "Gaussian: GLM vs Survey") +
-  geom_text_repel(data = subset(gsData, yPoint == max(yPoint)),
+  geom_text_repel(data = subset(gsData, yPoint %in% gsData$yPoint[gsInd][32:34] | yPoint %in% gsData$yPoint[!gsInd][32:34]),
                   mapping = aes(label = skill))
+  #geom_text_repel(data = subset(gsData, yPoint == max(yPoint)),
+  #                mapping = aes(label = skill))
 gs_graph
 
 # BLM vs Survey for Binomial Distro
 bsData <- rbind(bSvyData, bData)
+bsData <- bsData[order(bsData$yPoint),]
+bsInd <- bsData$type == 'LSVY'
 
 bs <- ggplot(data = bsData,
              mapping = aes(x = xPoint, y = yPoint, color = type))
 
 bs_graph <- bs + geom_point() + 
   labs(x = "x", y = "Expected Difference", title = "Binomial: GLM vs Survey") +
-  geom_text_repel(data = subset(bsData, yPoint == max(yPoint)),
+  geom_text_repel(data = subset(bsData, yPoint %in% bsData$yPoint[bsInd][32:34] | yPoint %in% bsData$yPoint[!bsInd][32:34]),
                   mapping = aes(label = skill))
+  #geom_text_repel(data = subset(bsData, yPoint == max(yPoint)),
+  #               mapping = aes(label = skill))
 bs_graph
+
 # gauss <- models.gauss[[2]]
 # binom <- models.binom[[1]]
 #
@@ -308,4 +323,3 @@ bs_graph
 # v2 <- as.data.frame(v)
 #
 # plot(df.reduced$win_num, v2$link)
-
